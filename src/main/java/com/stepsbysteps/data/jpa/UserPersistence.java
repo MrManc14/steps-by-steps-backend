5d1396d30c17805784e43917d044b422971119a0
macmilll <lucas.macmillan@cdk.com>
2019-08-06 07:32:23 -0700

    Fixed return type.

--- src/main/java/com/stepsbysteps/data/jpa/UserPersistence.java
+++ src/main/java/com/stepsbysteps/data/jpa/UserPersistence.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserPersistence extends JpaRepository<User, Long> {
-    boolean findByUserName(String userName);
+    Boolean findByUserName(String userName);
 }
