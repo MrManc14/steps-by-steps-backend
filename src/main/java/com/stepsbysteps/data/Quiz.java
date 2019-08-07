fbf7b7af4802771097d1234e4070949fe5037cee
macmilll <lucas.macmillan@cdk.com>
2019-08-07 13:33:03 -0700

    Added columns to quiz.

--- src/main/java/com/stepsbysteps/data/Quiz.java
+++ src/main/java/com/stepsbysteps/data/Quiz.java
@@ -14,8 +14,23 @@ public class Quiz {
     @Column(name = "userID")
     private String userID;
 
-    @Column(name = "score")
-    private String score;
+    @Column(name = "totalPoints")
+    private int totalPoints;
+
+    @Column(name = "transportPoints")
+    private int transportPoints;
+
+    @Column(name = "dietPoints")
+    private int dietPoints;
+
+    @Column(name = "shoppingPoints")
+    private int shoppingPoints;
+
+    @Column(name = "wastePoints")
+    private int wastePoints;
+
+    @Column(name = "powerPoints")
+    private int powerPoints;
 
     @Column(name = "timestamp")
     private Long timestamp;
@@ -36,12 +51,52 @@ public class Quiz {
         this.userID = userID;
     }
 
-    public String getScore() {
-        return score;
+    public int getScore() {
+        return totalPoints;
     }
 
-    public void setScore(String score) {
-        this.score = score;
+    public void setScore(int score) {
+        this.totalPoints = score;
+    }
+
+    public int getTransportPoints() {
+        return transportPoints;
+    }
+
+    public void setTransportPoints(int transportPoints) {
+        this.transportPoints = transportPoints;
+    }
+
+    public int getDietPoints() {
+        return dietPoints;
+    }
+
+    public void setDietPoints(int dietPoints) {
+        this.dietPoints = dietPoints;
+    }
+
+    public int getShoppingPoints() {
+        return shoppingPoints;
+    }
+
+    public void setShoppingPoints(int shoppingPoints) {
+        this.shoppingPoints = shoppingPoints;
+    }
+
+    public int getWastePoints() {
+        return wastePoints;
+    }
+
+    public void setWastePoints(int wastePoints) {
+        this.wastePoints = wastePoints;
+    }
+
+    public int getPowerPoints() {
+        return powerPoints;
+    }
+
+    public void setPowerPoints(int powerPoints) {
+        this.powerPoints = powerPoints;
     }
 
     public Long getTimestamp() {
@@ -52,10 +107,10 @@ public class Quiz {
         this.timestamp = timestamp;
     }
 
-    public Quiz(Long id, String userID, String score) {
+    public Quiz(Long id, String userID, int score) {
         this.id = id;
         this.userID = userID;
-        this.score = score;
+        this.totalPoints = score;
     }
 
     protected Quiz() {
